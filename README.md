e1e5345dda2e603be3c796ed08a7d7b4055867a0
fuxiaochen <aifuxi.js@gmail.com>
2024-02-11 23:58:28 +0800

    feat: 🎸 添加 Gin 同时支持热重载

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # fuxiaochen-api-with-go
 
 使用 Gin + GORM 构建 fuxiaochen.com 的 API
+
+## 前置准备
+
+- Go
+- [Air](https://github.com/cosmtrek/air)
+- Docker
+
+## 开发
+
+1. 安装依赖
+ ```shell
+go mod tidy
+ ```
+2. 启动开发环境，使用 [Air](https://github.com/cosmtrek/air) 自动热重载
+ ```shell
+air
+ ```
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := r.Run("127.0.0.1:6122")
+	if err != nil {
+		log.Fatalf("启动服务器失败：%v", err)
+	}
 }
